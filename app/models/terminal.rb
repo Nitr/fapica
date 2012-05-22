5a97546c6b248fc32703b0c969303ec7633c4359
Nitr <anty1989@mail.ru>
2012-05-22 16:20:16 +0400

    бд

---
+++ app/models/terminal.rb
@@ -0,0 +1,3 @@
+class Terminal < ActiveRecord::Base
+  belongs_to :company
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,40 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120518133007) do
+ActiveRecord::Schema.define(:version => 20120522114904) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "devices", :force => true do |t|
+    t.integer  "terminal_id"
+    t.integer  "type"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "devices", ["terminal_id"], :name => "index_devices_on_terminal_id"
+
+  create_table "keycodes", :force => true do |t|
+    t.string   "keycode"
+    t.boolean  "temp"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "logs", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "visit"
+    t.integer  "visit_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "logs", ["user_id"], :name => "index_logs_on_user_id"
 
   create_table "pages", :force => true do |t|
     t.string   "name"
@@ -31,6 +64,16 @@ ActiveRecord::Schema.define(:version => 20120518133007) do
     t.integer "role_id"
     t.integer "user_id"
   end
+
+  create_table "terminals", :force => true do |t|
+    t.string   "hashkey"
+    t.string   "name"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "terminals", ["company_id"], :name => "index_terminals_on_company_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
