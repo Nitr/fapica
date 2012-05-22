1f020af11b9e77aefda823d56e1cfe4b951b18dc
Nitr <anty1989@mail.ru>
2012-05-22 17:27:40 +0400

    get_hash пустой

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,12 @@
 class ApiController < ApplicationController
 	def get_hash
 		terminal = Terminal.find_by_hashkey(params[:hash])
-		if terminal.empty?
-			render :json => {error: 'Terminal not found'}		
-		else
-			render :json => terminal.hashkey 
-		end
+		#if terminal.empty?
+		#	render :json => {error: 'Terminal not found'}		
+		#else
+		#	render :json => terminal.hashkey 
+		#end
+		render :json => terminal
 	end
 
 	def set_hash
