4900ac9264157a81f8d7c9ddd210c7ba3e6c9d78
Nitr <anty1989@mail.ru>
2012-05-28 11:51:06 +0400

    api remake

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
   has_many :logs
   belongs_to :company
+  belongs_to :keycode
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-
-  attr_accessor :session_id
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -13,7 +12,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
-
   
   def role?(role)
     return !!self.roles.find_by_name(role.to_s.camelize)
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,12 @@ SpineRails3::Application.routes.draw do
   
   root :to => 'pages#index'
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
-  match 'api/get_hash/:hash' => 'api#get_hash', :via => :get
-  match 'api/set_hash' => 'api#set_hash', :via => :post
   match 'api/get_user_role' => 'api#get_user_role'
   match 'api/users' => 'api#users'
-  match 'api/user' => 'api#user_update', :via => :post
   match 'api/logs' => 'api#logs'
+
+  match 'api/get_hash/:hash' => 'api#get_hash', :via => :get
+  match 'api/set_hash' => 'api#set_hash', :via => :post
+  
+  match 'api/user_activate/:id/:enter' => 'api#user_activate', :via => :get
 end
